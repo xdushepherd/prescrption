c86de51f37874d41c4ce570c0ea8fa08998d85c9
王千一 <xdushepherd@gmail.com>
2014-01-30 00:31:22 +0800

    保存修改

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,14 @@
 //= require select2
 //= require turbolinks
 //= require_tree .
+function remove_fields(link) {
+        $(link).closest("input[type=hidden]").value = "1";
+        $(link).closest("tr").hide();
+}
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g");
+  $(link).up().insert({
+        before: content.replace(regexp, new_id)
+  });
+}
